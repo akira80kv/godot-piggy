1edbcfde31bca839a1204c95128c6ec64ba76897
akira80kv <61351191+akira80kv@users.noreply.github.com>
2020-02-23 17:35:52 +0100

    Refactor SPEED constant

--- Piggy.gd
+++ Piggy.gd
@@ -1,14 +1,16 @@
 extends Area2D
 
+const SPEED = 4
+
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
-		move(4, 0)
+		move(SPEED, 0)
 	if Input.is_action_pressed("ui_left"):
-		move(-4, 0)
+		move(-SPEED, 0)
 	if Input.is_action_pressed("ui_down"):
-		move(0, +4)
+		move(0, SPEED)
 	if Input.is_action_pressed("ui_up"):
-		move(0, -4)
+		move(0, -SPEED)
 	
 func move(xspeed, yspeed):
 		position.x += xspeed
