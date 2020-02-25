6e6ebe08c5a751c99c8ff7ba88616b65d2d57c66
akira80kv <61351191+akira80kv@users.noreply.github.com>
2020-02-25 20:01:27 +0100

    Movement improved

--- Piggy.gd
+++ Piggy.gd
@@ -4,13 +4,16 @@ export(int) var SPEED = 50
 var moving = false
 
 onready var animationPlayer = $AnimationPlayer
+onready var sprite = $Sprite
 
 func _process(delta):
 	moving = false
 	if Input.is_action_pressed("ui_right"):
 		move(SPEED, 0, delta)
+		sprite.flip_h = false
 	if Input.is_action_pressed("ui_left"):
 		move(-SPEED, 0, delta)
+		sprite.flip_h = true
 	if Input.is_action_pressed("ui_down"):
 		move(0, SPEED, delta)
 	if Input.is_action_pressed("ui_up"):
