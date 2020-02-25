a2d5b4a1344f19ec8826a376ffdef4efb10eabd7
akira80kv <61351191+akira80kv@users.noreply.github.com>
2020-02-25 19:55:07 +0100

    Animation fixed

--- Piggy.gd
+++ Piggy.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
 export(int) var SPEED = 50
+var moving = false
 
 onready var animationPlayer = $AnimationPlayer
 
 func _process(delta):
+	moving = false
 	if Input.is_action_pressed("ui_right"):
 		move(SPEED, 0, delta)
 	if Input.is_action_pressed("ui_left"):
@@ -13,8 +15,13 @@ func _process(delta):
 		move(0, SPEED, delta)
 	if Input.is_action_pressed("ui_up"):
 		move(0, -SPEED, delta)
+	if moving == true:
+		animationPlayer.play("Run")
+	else:
+		animationPlayer.play("Idle")
+		
 	
 func move(xspeed, yspeed, delta):
 		position.x += xspeed * delta
 		position.y += yspeed * delta
-		animationPlayer.play("Run")
+		moving = true
